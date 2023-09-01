c30dce24851fb08f380348427b7f5693dd5c8bb3
mjulialobo <mjulialobo23@gmail.com>
2023-09-01 15:37:17 -0300

    refactor(home): add limit variable

--- src/screens/Home/HomeDetails.tsx
+++ src/screens/Home/HomeDetails.tsx
@@ -17,6 +17,7 @@ import {
   BANNER_TITLE,
   MOST_WANTED,
   MOST_WANTED_COMPLEMENT,
+  PRODUCTS_LIMIT,
 } from "./constants";
 
 export type HomeDetailsProps = {
@@ -25,53 +26,51 @@ export type HomeDetailsProps = {
 
 export const HomeDetails = ({ products }: HomeDetailsProps) => {
   return (
-    <>
-      <ContentContainer>
-        <Banner>
-          <Image
-            src="https://images.unsplash.com/photo-1623625434531-d130448273c1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80"
-            data-testid="banner"
-            width={800}
-            height={400}
-            alt="banner"
-            className="banner"
-          />
-          <BannerTextContent>
-            <BannerTitle>{BANNER_TITLE}</BannerTitle>
-            <Text>{BANNER_TEXT}</Text>
-            <Link href="/">
-              <Image
-                src="https://cdn-icons-png.flaticon.com/128/318/318476.png"
-                data-testid="navigate-icon"
-                width={30}
-                height={30}
-                alt="navigate-icon"
-              />
-            </Link>
-          </BannerTextContent>
-        </Banner>
-        <ProductDisplay>
-          <ProductDisplayTextContent>
-            <ProductDisplayTitle>{MOST_WANTED}</ProductDisplayTitle>
-            <Text>{MOST_WANTED_COMPLEMENT}</Text>
-          </ProductDisplayTextContent>
-          {products.map(
-            (product, index) =>
-              index <= 3 && <ProductCard key={index} product={product} />
-          )}
-
+    <ContentContainer>
+      <Banner>
+        <Image
+          src="https://images.unsplash.com/photo-1623625434531-d130448273c1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80"
+          data-testid="banner"
+          width={800}
+          height={400}
+          alt="banner"
+          className="banner"
+        />
+        <BannerTextContent>
+          <BannerTitle>{BANNER_TITLE}</BannerTitle>
+          <Text>{BANNER_TEXT}</Text>
           <Link href="/">
             <Image
-              src="https://cdn-icons-png.flaticon.com/128/6318/6318306.png"
+              src="https://cdn-icons-png.flaticon.com/128/318/318476.png"
               data-testid="navigate-icon"
-              width={60}
-              height={60}
+              width={30}
+              height={30}
               alt="navigate-icon"
-              className="navigate-icon"
             />
           </Link>
-        </ProductDisplay>
-      </ContentContainer>
-    </>
+        </BannerTextContent>
+      </Banner>
+      <ProductDisplay>
+        <ProductDisplayTextContent>
+          <ProductDisplayTitle>{MOST_WANTED}</ProductDisplayTitle>
+          <Text>{MOST_WANTED_COMPLEMENT}</Text>
+        </ProductDisplayTextContent>
+        {products.map(
+          (product, index) =>
+            index <= PRODUCTS_LIMIT && (
+              <ProductCard key={index} product={product} />
+            )
+        )}
+        <Link href="/">
+          <Image
+            src="https://cdn-icons-png.flaticon.com/128/6318/6318306.png"
+            data-testid="navigate-icon"
+            width={60}
+            height={60}
+            alt="navigate-icon"
+          />
+        </Link>
+      </ProductDisplay>
+    </ContentContainer>
   );
 };
--- src/screens/Home/constants.ts
+++ src/screens/Home/constants.ts
@@ -5,3 +5,5 @@ export const MOST_WANTED_COMPLEMENT = 'Os produtos mais procurados pelos nossos 
 export const BANNER_TITLE = 'Área Externa';
 
 export const BANNER_TEXT = 'Veja as novidades em decoração para tornar sua área externa aconchegante e convidativa.';
+
+export const PRODUCTS_LIMIT = 3;
