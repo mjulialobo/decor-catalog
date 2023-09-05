390891da130b16b27db7dee6952ed896b839479d
mjulialobo <mjulialobo23@gmail.com>
2023-09-05 00:29:48 -0300

    feat(navdetail): remove console

--- src/screens/Navigation/NavigationDetails.tsx
+++ src/screens/Navigation/NavigationDetails.tsx
@@ -38,7 +38,6 @@ export const NavigationDetails = ({ products }: NavigationProps) => {
 
   const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
-    console.log(event.target.value);
   };
 
   return (
